8504ea65020c0d99a03295dfe17ba1e16409292b
NNDuy-UIT <nguuennamduy1372007@gmail.com>
2025-11-29 13:04:09 +0700

    Add files via upload

---
+++ xep_gach.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+#include <climits>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <cmath>
+using namespace std;
+
+long long mod = 1e9 + 7;
+
+bool cmp(int x, int y){
+    return x > y;
+}
+
+int main(){
+    int n; cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    sort(a, a + n, cmp);
+    vector<int> re = {a[0]};
+    int strength = a[0];
+    for (int i = 1; i < n; i++){
+        --strength;
+        if (a[i] < strength){
+            strength = a[i];
+        }
+        re.push_back(a[i]);
+        if (strength == 0){
+            break;
+        }
+
+    }
+    cout << re.size();
+    return 0;
+}
