142aec347272227df81f02bf35431f945157ad50
NNDuy-UIT <nguuennamduy1372007@gmail.com>
2025-12-04 08:38:29 +0700

    Add files via upload

---
+++ tan_suat.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <iomanip>
+#include <climits>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <cmath>
+using namespace std;
+
+long long mod = 1e9 + 7;
+
+
+
+
+int main(){
+    int n; cin >> n;
+    map<int, int> mp;
+    vector<int> a;
+    vector<int> b;
+    for (int i = 0; i < n; i++){
+        int value; cin >> value;
+        a.push_back(value);
+        b.push_back(value);
+        mp[value]++;
+    }
+    sort(a.begin(), a.end(), [&mp](int a, int b){
+        if (mp[a] != mp[b]){
+            return mp[a] > mp[b];
+        }
+        else{
+            return a < b;
+        }
+    });
+    for (auto value : a){
+        cout << value << " ";
+    }
+    cout << endl;
+    stable_sort(b.begin(), b.end(), [&mp](int a, int b){
+        return mp[a] > mp[b];
+    });
+    for (auto value : b){
+        while (mp[value] > 0){
+            cout << value << " ";
+            mp[value]--;
+        }
+    }
+    return 0;
+}
